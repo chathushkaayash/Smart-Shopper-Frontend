a9d39e1246f13affa83074e2010e3c569452f0fc
chathushkaayash <chathushkaayash@gmail.com>
2024-06-15 20:36:23 +0000

    See https://github.com/UCSC-SmartShopper/frontend/commit/2890c5cfb517bac7102422db69a3fd77552401de

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from "./pages/Login";
+import CartDetails from "./pages/CartDetails";
 
 function App() {
   return (
     <>
+    <Router>
       <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<CartDetails />} />
+      </Routes>
       
-      <Login />
+    </Router>
+      
     </>
   );
 }
