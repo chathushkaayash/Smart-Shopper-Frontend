2d41e1669c0333ebcdcdcff1678858e91a1970ad
chathushkaayash <chathushkaayash@gmail.com>
2024-12-02 17:28:06 +0000

    See https://github.com/UCSC-SmartShopper/frontend/commit/097e45c20ea326890e4caf99a990579ab8c6d46d

--- src/services/UserPreference/useCreateUserPreference.ts
+++ src/services/UserPreference/useCreateUserPreference.ts
@@ -1,32 +1,17 @@
 import APIClient from "@/services/api-client";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
+import { useMutation } from "@tanstack/react-query";
 
 export interface UserPreference {
-    userId: number;
-    preferenceType: string;
-    referenceId: number;
+  userId: number;
+  preferenceType: string;
+  referenceId: number;
 }
 
 const apiClient = new APIClient<UserPreference>("/userpreference/add");
 
 const useCreateUserPreference = () => {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: (data: Omit<UserPreference, "id">) => apiClient.create(data),
-
-    onSuccess: () => {
-      toast.success("User preference added successfully");
-    },
-
-    onError: () => {
-      toast.error("An error occurred while adding User preference");
-    },
-
-    onSettled: () => {
-      queryClient.resetQueries({ queryKey: ["cart_items"] });
-    },
   });
 };
 
